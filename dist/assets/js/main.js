237941697162dfc6dbf4d98422c40d6ea06bdbde
andreinaypp <andreinaypp88@gmail.com>
2021-07-31 21:33:26 -0400

    home lista, revisar jon

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -3,6 +3,9 @@
 // ------------------------------------------------------------
 $(window).ready(function () {
   changeColorNavbar();
+
+  tabcontentInitial = document.querySelector(".tabcontent.tab-ppal");
+  tabcontentInitial.style.display = "block";
 });
 $(window).scroll(function () {
   changeColorNavbar();
@@ -24,4 +27,28 @@ function changeColorNavbar() {
       "background-color": "#90ccc6aa",
     });
   }
+}
+
+$(function () {
+  $(".box").click(function () {
+    $("html, body").animate(
+      { scrollTop: $("section.section2").offset().top },
+      "slow"
+    );
+    return false;
+  });
+});
+
+function openCity(evt, cardMenus) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.querySelectorAll(".tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.querySelectorAll(".tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cardMenus).style.display = "block";
+  evt.currentTarget.className += " active";
 }
