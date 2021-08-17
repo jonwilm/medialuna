1dbd30421f5993e87519c42ca86b15af02ae58f0
jonwilm <jparedes84@gmail.com>
2021-08-17 16:05:41 -0400

    dragabble cesta

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -141,3 +141,27 @@ $('#close-shopping-cart').click(function() {
   $('#shopping-cart').css('right', 'calc(-' + shoppingCartW + 'px - 2rem')
   shoppingCart = 'close'
 })
+
+const slider = document.querySelector('#cart-sugeridos');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+});
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 1; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+});
