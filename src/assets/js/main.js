12484fd9f519c8724a6a183ccdb452ce730ac3e4
andreinaypp <andreinaypp88@gmail.com>
2021-09-10 15:45:59 -0400

    menu semanal

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,7 +4,7 @@
 $(window).ready(function () {
   // changeColorNavbar();
   changeDesktopNavbar();
-  
+
   tabcontentInitial = document.querySelector(".tabcontent.tab-ppal");
   tabcontentInitial.style.display = "block";
 });
@@ -18,11 +18,13 @@ $(window).resize(function () {
 function changeColorNavbar() {
   if ($(window).scrollTop() > 100) {
     $(".navbar").css({
-      "background": "linear-gradient(87.71deg, rgba(189, 231, 226, 0.75) 0%, #9CD3CD 46%, rgba(156, 211, 205, 0.76) 100%)",
+      background:
+        "linear-gradient(87.71deg, rgba(189, 231, 226, 0.75) 0%, #9CD3CD 46%, rgba(156, 211, 205, 0.76) 100%)",
     });
   } else {
     $(".navbar").css({
-      "background-color": "linear-gradient(87.71deg, rgba(189, 231, 226, 0.75) 0%, #9CD3CD 46%, rgba(156, 211, 205, 0.76) 100%)",
+      "background-color":
+        "linear-gradient(87.71deg, rgba(189, 231, 226, 0.75) 0%, #9CD3CD 46%, rgba(156, 211, 205, 0.76) 100%)",
     });
   }
 }
@@ -30,47 +32,50 @@ function changeDesktopNavbar() {
   if ($(window).width() >= 1200) {
     if ($(window).scrollTop() > 100) {
       $("#pre-nav").css({
-        "height": "0px",
+        height: "0px",
       });
       setTimeout(() => {
         $("#pre-nav > div").css({
-          "opacity": "0",
+          opacity: "0",
         });
       }, 300);
       $(".navbar").css({
-        "height": "55px",
+        height: "55px",
       });
       $(".logo-navbar").css({
-        "transform": "translateY(0px)",
+        transform: "translateY(0px)",
       });
     } else {
       $("#pre-nav").css({
-        "height": "20px",
+        height: "20px",
       });
       setTimeout(() => {
         $("#pre-nav > div").css({
-          "opacity": "1",
+          opacity: "1",
         });
       }, 300);
       $(".navbar").css({
-        "height": "35px",
+        height: "35px",
       });
       $(".logo-navbar").css({
-        "transform": "translateY(-10px)",
+        transform: "translateY(-10px)",
       });
     }
   } else {
     $(".navbar").css({
-      "height": "auto",
+      height: "auto",
     });
     $(".logo-navbar").css({
-      "transform": "translateY(0px)",
+      transform: "translateY(0px)",
     });
   }
 }
 
-$(".box").click(function() {
-  $("html, body").animate({ scrollTop: $("section.section2").offset().top }, "slow");
+$(".box").click(function () {
+  $("html, body").animate(
+    { scrollTop: $("section.section2").offset().top },
+    "slow"
+  );
   return false;
 });
 
@@ -88,11 +93,11 @@ function openCity(evt, cardMenus) {
   evt.currentTarget.className += " active";
 }
 
-$('#mas, #menos').click("on", function(e) {
+$("#mas, #menos").click("on", function (e) {
   e.preventDefault();
-  let cantidad = $(this).siblings('.cantidad');
-  let min = parseInt(cantidad.attr('min'));
-  let max = parseInt(cantidad.attr('max'));
+  let cantidad = $(this).siblings(".cantidad");
+  let min = parseInt(cantidad.attr("min"));
+  let max = parseInt(cantidad.attr("max"));
   let current_value = parseInt(cantidad.val());
   let classname = $(this).attr("id");
   if (classname === "menos") {
@@ -108,64 +113,64 @@ $('#mas, #menos').click("on", function(e) {
       cantidad.val(current_value + 1);
     }
   }
-})
-$('.cantidad').change(function() {
-  let min = parseInt($(this).attr('min'));
-  let max = parseInt($(this).attr('max'));
+});
+$(".cantidad").change(function () {
+  let min = parseInt($(this).attr("min"));
+  let max = parseInt($(this).attr("max"));
   if (parseInt($(this).val()) < min) {
-    $(this).val(min)
+    $(this).val(min);
   } else if (parseInt($(this).val()) > max) {
-    $(this).val(max)
+    $(this).val(max);
   }
-})
+});
 
-let shoppingCartW = $('#shopping-cart').width()
-$(document).ready(function() {
-  shoppingCartW = $('#shopping-cart').width()
-  $('#shopping-cart').css('right', 'calc(-' + shoppingCartW + 'px - 2rem')
-})
-$(window).resize(function() {
-  shoppingCartW = $('#shopping-cart').width()
-})
-let shoppingCart = 'close'
-$('#btn-shopping-cart').click(function() {
-  if (shoppingCart == 'close') {
-    $('#shopping-cart').css('right', '0%')
-    shoppingCart = 'open'
+let shoppingCartW = $("#shopping-cart").width();
+$(document).ready(function () {
+  shoppingCartW = $("#shopping-cart").width();
+  $("#shopping-cart").css("right", "calc(-" + shoppingCartW + "px - 2rem");
+});
+$(window).resize(function () {
+  shoppingCartW = $("#shopping-cart").width();
+});
+let shoppingCart = "close";
+$("#btn-shopping-cart").click(function () {
+  if (shoppingCart == "close") {
+    $("#shopping-cart").css("right", "0%");
+    shoppingCart = "open";
   } else {
-    $('#shopping-cart').css('right', 'calc(-' + shoppingCartW + 'px - 2rem')
-    shoppingCart = 'close'
+    $("#shopping-cart").css("right", "calc(-" + shoppingCartW + "px - 2rem");
+    shoppingCart = "close";
   }
-})
-$('#close-shopping-cart').click(function() {
-  $('#shopping-cart').css('right', 'calc(-' + shoppingCartW + 'px - 2rem')
-  shoppingCart = 'close'
-})
+});
+$("#close-shopping-cart").click(function () {
+  $("#shopping-cart").css("right", "calc(-" + shoppingCartW + "px - 2rem");
+  shoppingCart = "close";
+});
 
-const slider = document.querySelector('#cart-sugeridos');
+const slider = document.querySelector("#cart-sugeridos");
 let isDown = false;
 let startX;
 let scrollLeft;
 
-slider.addEventListener('mousedown', (e) => {
+slider.addEventListener("mousedown", (e) => {
   isDown = true;
   startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
 });
-slider.addEventListener('mouseleave', () => {
+slider.addEventListener("mouseleave", () => {
   isDown = false;
 });
-slider.addEventListener('mouseup', () => {
+slider.addEventListener("mouseup", () => {
   isDown = false;
 });
-slider.addEventListener('mousemove', (e) => {
-  if(!isDown) return;
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
   e.preventDefault();
   const x = e.pageX - slider.offsetLeft;
   const walk = (x - startX) * 1; //scroll-fast
   slider.scrollLeft = scrollLeft - walk;
 });
 
-$(window).ready(function() {
-  $('#modal-suscribe').modal('show')
-})
+$(window).ready(function () {
+  $("#modal-suscribe").modal("show");
+});
